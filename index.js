dc384fc598ece6e5aa26d862081309786a8ce201
Zack Davis <Zack@octopodainteractive.com>
2024-08-12 11:36:11 -0500

    added sentry to the test app

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
+// const Rollbar = require('rollbar')
+const Sentry = require('@sentry/node')
+const { nodeProfilingIntegration } = require('@sentry/profiling-node')
+
+
+// var rollbar = new Rollbar({
+//     accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+//     captureUncaught: true,
+//     captureUnhandledRejections: true,
+// });
+
+Sentry.init({ 
+    dsn:  process.env.SENTRY_DSN,
+    integrations: [
+        nodeProfilingIntegration()
+    ],
+    tracesSampleRate: 1.0,
+    profilesSampleRate: 1.0
+})
+
+
+
+
 const express = require('express');
 const path = require('path');
-const Rollbar = require('rollbar')
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-var rollbar = new Rollbar({
-    accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
-    captureUncaught: true,
-    captureUnhandledRejections: true,
-});
 
 app.use(express.json());
 
@@ -20,11 +36,14 @@ app.post('/log-error', (req, res) => {
     console.log('type', errorType);
 
     if (errorType === 'unhandledException') {
-        rollbar.error(`Unhandled Exception: ${message}`);
+        // rollbar.error(`Unhandled Exception: ${message}`);
+        Sentry.captureException(new Error(`Unhandled Exception: ${message}`))
     } else if (errorType === 'unhandledRejection') {
-        rollbar.error(`Unhandled Rejection: ${message}`);
+        // rollbar.error(`Unhandled Rejection: ${message}`);
+        Sentry.captureException(new Error(`Unhandled Rejection: ${message}`))
     } else if (errorType === 'handledException') {
-        rollbar.info(`Handled Exception: ${message}`);
+        // rollbar.info(`Handled Exception: ${message}`);
+        Sentry.captureMessage(new Error(`Handled Exception: ${message}`))
     }
 
     res.status(200).send('Error logged to Rollbar');
@@ -33,6 +52,9 @@ app.post('/log-error', (req, res) => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+//Error Handler for Sentry
+Sentry.setupExpressErrorHandler(app);
 
 
 app.listen(PORT, () => {
