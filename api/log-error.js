1578c315d1f53d8536911c86e2a783b3c7958073
Zack Davis <Zack@octopodainteractive.com>
2024-08-12 13:51:08 -0500

    updated log error

--- api/log-error.js
+++ api/log-error.js
@@ -1,28 +1,29 @@
 const Sentry = require('@sentry/node');
-const { nodeProfilingIntegration } = require('@sentry/profiling-node')
+
 
 // Initialize Sentry with your DSN (Data Source Name)
-Sentry.init({ 
-    dsn:  process.env.SENTRY_DSN,
-    integrations: [
-        nodeProfilingIntegration()
-    ],
-    tracesSampleRate: 1.0,
-    profilesSampleRate: 1.0
-})
-
-
+Sentry.init({ dsn: process.env.SENTRY_DSN });
 
 export default (req, res) => {
-    const { message, errorType } = req.body;
+    try {
+        const { message, errorType } = req.body;
 
-    if (errorType === 'unhandledException') {
-        Sentry.captureException(new Error(`Unhandled Exception: ${message}`));
-    } else if (errorType === 'unhandledRejection') {
-        Sentry.captureException(new Error(`Unhandled Rejection: ${message}`));
-    } else if (errorType === 'handledException') {
-        Sentry.captureMessage(`Handled Exception: ${message}`, 'info');
+        if (!message || !errorType) {
+            throw new Error('Missing message or errorType');
+        }
+
+        if (errorType === 'unhandledException') {
+            Sentry.captureException(new Error(`Unhandled Exception: ${message}`));
+        } else if (errorType === 'unhandledRejection') {
+            Sentry.captureException(new Error(`Unhandled Rejection: ${message}`));
+        } else if (errorType === 'handledException') {
+            Sentry.captureMessage(`Handled Exception: ${message}`, 'info');
+        }
+
+        res.status(200).send('Error logged to Sentry');
+    } catch (err) {
+        console.error('Error in API function:', err);
+        Sentry.captureException(err);  // Log this internal error to Sentry
+        res.status(500).send('Internal Server Error');
     }
-
-    res.status(200).send('Error logged to Sentry');
 };
